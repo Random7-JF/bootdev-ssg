eafdddbd9e9a1e911af41140a4f5e118b1ed48ab
Jon Fingas <jon.fingas@gmail.com>
2024-05-30 22:44:51 -0500

    initial

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from textnode import TextNode
+
+test = TextNode("This is a text node", "bold", "https://www.boot.dev")
+print(test)
---
+++ src/textnode.py
@@ -0,0 +1,16 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, value: object) -> bool:
+        return (
+            self.text_type == value.text_type
+            and self.text == value.text
+            and self.url == value.url           
+        )
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
